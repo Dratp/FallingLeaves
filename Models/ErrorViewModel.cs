06e5fa2f5031eab8b70a14750e8c65ab0b9ef53a
David Heizer <DavidHeizer@QuickenLoans.com>
2020-11-02 17:20:19 -0500

    Falling leaves

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Falling_leaves.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
